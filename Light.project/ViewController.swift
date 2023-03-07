224386eee5f73696420191f79bf42ff7232d63e0
Armando <avaquerwks10@gmail.com>
2023-03-07 10:22:47 -0500

    Initial Commit

---
+++ Light.project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Light.project
+//
+//  Created by Armando Vaquer on 3/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
